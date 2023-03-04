c439bfec3b7c54e7b66b2b487ecfb37b74535812
lucascbb <lucasbaroquello@hotmail.com>
2023-03-04 02:43:50 -0300

    Req 2 - 7/7

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    id: { 
+      type: DataTypes.INTEGER, 
+      primaryKey: true, 
+      autoIncrement: true 
+    },
     display_name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
