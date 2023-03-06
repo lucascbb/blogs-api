c1eeddee7b9279e41b0bb13f8481056993f96608
lucascbb <lucasbaroquello@hotmail.com>
2023-03-06 10:57:24 -0300

    Req 5 completo

--- src/controllers/allUsers.controller.js
+++ src/controllers/allUsers.controller.js
@@ -3,6 +3,5 @@ const { createUserService } = require('../services');
 
 module.exports = async (req, res) => {
   const allUsers = await createUserService.getAllUsers();
-
   return res.status(200).json(allUsers);
 };
