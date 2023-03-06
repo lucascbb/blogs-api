ef295c3000829eab3f9657b5e3960bb464c87479
lucascbb <lucasbaroquello@hotmail.com>
2023-03-06 06:30:26 -0300

    Req 4 - validates

--- src/controllers/createUser.controller.js
+++ src/controllers/createUser.controller.js
@@ -1,13 +1,20 @@
 require('dotenv/config');
 const { createUserService } = require('../services');
 const tokenJWT = require('../Utils/pwt');
+const { createUserValidate } = require('../services/validators/createUser.Validate');
 
 module.exports = async (req, res) => {
   const data = req.body;
 
-  await createUserService.newUser(data);
-  const { id } = await createUserService.getIdbyEmail(data.email);
+  const hasId = await createUserService.getIdbyEmail(data.email);
+  const result = createUserValidate(data, hasId);
 
+  if (result) { return res.status(result.status).json({ message: result.message }); }
+
+  await createUserService.newUser(data);
+  
+  const { id } = await createUserService.getIdbyEmail(data.email);
   const token = tokenJWT(id);
+
   return res.status(200).json({ token });
 };
---
+++ src/services/validators/createUser.Validate.js
@@ -0,0 +1,25 @@
+const createUserValidate = (data, user) => {
+  const regexEmail = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/;
+  console.log(user);
+  if (data.displayName.length < 8) { 
+    return { status: 400, message: '"displayName" length must be at least 8 characters long' }; 
+  }
+
+  if (!regexEmail.test(data.email)) { 
+    return { status: 400, message: '"email" must be a valid email' }; 
+  }
+
+  if (data.password.length < 6) { 
+    return { status: 400, message: '"password" length must be at least 6 characters long' }; 
+  }
+
+  if (user) { 
+    return { status: 409, message: 'User already registered' }; 
+  }
+
+  return null;  
+};
+
+module.exports = {
+  createUserValidate,
+};
