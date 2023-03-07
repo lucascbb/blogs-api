b52db7eb8dfb80169a5f7f2bc85fe7d1d725ea2d
lucascbb <lucasbaroquello@hotmail.com>
2023-03-06 22:34:55 -0300

    Req 9 - 3/3

--- src/controllers/allCategories.Controller.js
+++ src/controllers/allCategories.Controller.js
@@ -3,5 +3,5 @@ const { categoryService } = require('../services');
 
 module.exports = async (_req, res) => {
   const result = await categoryService.getAllCategories();
-  return res.status(201).json(result);
+  return res.status(200).json(result);
 };
