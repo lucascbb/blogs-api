c33580536290cd681c9f164194907c3bb9e58ab6
lucascbb <lucasbaroquello@hotmail.com>
2023-03-08 03:15:39 -0300

    Req 12 - completo

--- src/controllers/createBlogPost.Controller.js
+++ src/controllers/createBlogPost.Controller.js
@@ -6,22 +6,31 @@ module.exports = async (req, res) => {
   const { title, content, categoryIds } = req.body;
 
   const allCategories = await blogPostService.getAllCategories();
+
+  // Aqui uso um for para adicionar no array todos Ids que ja existem na tabela,
+  // valido caso ja exista pode usar ele como foreign key em outra tabela
   let arrId = [];
-  for (let i = 0; i < categoryIds.length; i += 1) { arrId = arrId.concat(allCategories
-    .map((ele) => ele.dataValues.id).some((ele2) => ele2 === categoryIds[i]));
+  for (let i = 0; i < categoryIds.length; i += 1) { 
+    arrId = arrId.concat(allCategories.map((ele) => ele.dataValues.id)
+    .some((ele2) => ele2 === categoryIds[i]));
   }
 
+  // Duas funcoes que verificam se ja existe algum item na tabela com mesmo conteudo que content e title
   const alredyAdd = await blogPostService.getAddedContent(content);
   const alredyAdd2 = await blogPostService.getAddedTitle(title);
 
+  // Verificamos se os itens passados pelo usuario estao de acordo
   const validate = createPostValidate(title, arrId, alredyAdd, alredyAdd2);
   if (validate) { return res.status(validate.status).json({ message: validate.message }); }
 
-  const obj = { title, content, userId: 1, published: Date.now(), updated: Date.now() };
-  await blogPostService.newBlogPost(obj);
+  // Aqui passamos um objeto para a funcao que popula a tabela blogpost
+  const itensToAdd = { title, content, userId: 1, published: Date.now(), updated: Date.now() };
+  await blogPostService.newBlogPost(itensToAdd);
 
+  // Aqui pegamos o dataValues pois dentro dele tem o ID do ultimo item adicionado
   const { dataValues } = await blogPostService.getAddedContent(content);
-
+  
+  // Aqui o retorno eh uma promises (igual o nome rs), e por isso usamos o Promise.All 
   const promises = [{ id: categoryIds[0] }, { id: categoryIds[1] }].map((category) => 
   blogPostService.newPostId({ postId: dataValues.id, categoryId: category.id }));
   await Promise.all(promises);
