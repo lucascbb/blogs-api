a565aa4287661dcb11dcbc9124ba55e76fdc0c76
lucascbb <lucasbaroquello@hotmail.com>
2023-03-08 04:19:27 -0300

    Req 13 - inicio 2

--- src/controllers/allBlogPosts.Controller.js
+++ src/controllers/allBlogPosts.Controller.js
@@ -4,6 +4,13 @@ const { blogPostService } = require('../services');
 module.exports = async (_req, res) => {
   const result = await blogPostService.allBlogPosts();
   const { dataValues } = await blogPostService.getUsersById(result.map((ele) => ele.userId));
+  // for (let i = 0; i < result.length; i += 1) { 
+  //   console.log(result[i].dataValues);
+  // }
+
+  console.log(result[0].dataValues);
   console.log(dataValues);
+  console.log([result[0].dataValues].concat(dataValues));
+
   return res.status(200).json(result);
 };
