ddbddfa75c4f340233cf18f4881869f94f5577a8
lucascbb <lucasbaroquello@hotmail.com>
2023-03-09 11:17:13 -0300

    ataualizando

--- src/controllers/deletePost.Controller.js
+++ src/controllers/deletePost.Controller.js
@@ -1,15 +1,15 @@
 require('dotenv/config');
 const { blogPostService } = require('../services');
-// const { validateToken } = require('../Utils/token.Validate');
+const { validateToken } = require('../Utils/token.Validate');
 const { deletePostIdValidate } = require('../services/validators/deletePost.Validate');
 
 module.exports = async (req, res) => {
   const { id } = req.params;
-  // const { authorization } = req.header;
+  const { authorization } = req.header;
 
   const idBlogPost = await blogPostService.idBlogPost(id);
 
-  // const currentID = validateToken(authorization).data.userId;
+  const currentID = validateToken(authorization).data.userId;
   // const idUserPost = idBlogPost[0].userId;
 
   const validate = deletePostIdValidate(idBlogPost);
--- src/controllers/editBlogPost.Controller.js
+++ src/controllers/editBlogPost.Controller.js
@@ -8,17 +8,18 @@ module.exports = async (req, res) => {
   const data = req.body;
   const { authorization } = req.headers;
 
-  // Atualizando o banco de dados
-  await blogPostService.updatePost(data, id);
   // Retornando o post baseado no Id 
-  const idBlogPost = await blogPostService.idBlogPost(id);
+  const idBlogPost0 = await blogPostService.idBlogPost(id);
 
   // As proximas 4 linhas de codigo sao para validar
   // Retorna o usuario que esta logado atraves do TOKEN
   const currentID = validateToken(authorization).data.userId;
-  const idUserPost = idBlogPost[0].userId;
-  const validate = editPostIdValidate(idBlogPost, currentID, idUserPost, data);
+  const idUserPost = idBlogPost0[0].userId;
+  const validate = editPostIdValidate(idBlogPost0, currentID, idUserPost, data);
   if (validate) { return res.status(validate.status).json({ message: validate.message }); } 
 
+  // Atualizando o banco de dados
+  await blogPostService.updatePost(data, id);
+  const idBlogPost = await blogPostService.idBlogPost(id);
   return res.status(200).json(idBlogPost[0].dataValues);
 };
