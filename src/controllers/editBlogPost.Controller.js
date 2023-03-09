af4776a6966a39aa5e6a0b657ac7d8ba0f53bd79
lucascbb <lucasbaroquello@hotmail.com>
2023-03-09 10:22:57 -0300

    Req 15 - inicio

--- src/controllers/editBlogPost.Controller.js
+++ src/controllers/editBlogPost.Controller.js
@@ -1,12 +1,21 @@
 require('dotenv/config');
-// const { blogPostService } = require('../services');
-// const { idPostIdValidate } = require('../services/validators/idBlogPost.Validate');
+const { validateToken } = require('../Utils/token.Validate');
+const { blogPostService } = require('../services');
+const { idPostIdValidate } = require('../services/validators/idBlogPost.Validate');
 
 module.exports = async (req, res) => {
   const { id } = req.params;
+  const data = req.body;
+  const { authorization } = req.headers;
 
-  // const validate = idPostIdValidate(idBlogPost);
-  // if (validate) { return res.status(validate.status).json({ message: validate.message }); } 
+  const idBlogPost = await blogPostService.idBlogPost(id);
 
-  return res.status(200).json(id);
+  const currentID = validateToken(authorization).data.userId;
+  const idUserPost = idBlogPost[0].userId;
+  const validate = idPostIdValidate(idBlogPost, currentID, idUserPost, data);
+  if (validate) { return res.status(validate.status).json({ message: validate.message }); } 
+
+  console.log();
+
+  return res.status(200).json(data);
 };
--- src/services/validators/idBlogPost.Validate.js
+++ src/services/validators/idBlogPost.Validate.js
@@ -1,5 +1,7 @@
-const idPostIdValidate = (id) => { 
-  if (!id) return { status: 404, message: 'Post does not exist' }; 
+const idPostIdValidate = (id, currentID, idUserPost, da) => { 
+  if (!id) return { status: 404, message: 'Post does not exist' };
+  if (currentID !== idUserPost) return { status: 401, message: 'Unauthorized user' };
+  if (!da.content || !da.title) return { status: 400, message: 'Some required fields are missing' };
 };
 
 module.exports = { idPostIdValidate };
