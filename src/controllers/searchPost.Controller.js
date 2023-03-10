c1ecc4ffa9c8167e8345e576a074f80844317047
lucascbb <lucasbaroquello@hotmail.com>
2023-03-10 12:03:45 -0300

    18 - completo

--- src/controllers/searchPost.Controller.js
+++ src/controllers/searchPost.Controller.js
@@ -4,7 +4,10 @@ const { blogPostService } = require('../services');
 module.exports = async (req, res) => {
   const { q } = req.query;
 
+  const lowerCase = q.toLowerCase();
   const inner = await blogPostService.allBlogPosts();
-  const result = inner.filter((ele) => ele.title.includes(q) || ele.content.includes(q));
+  const result = inner.filter((ele) => 
+    ele.title.toLowerCase().includes(lowerCase) || ele.content.toLowerCase().includes(lowerCase));
+
   return res.status(200).json(result);
 };
